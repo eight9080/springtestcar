021c01dcd7e6e093bd0ba9533c7ee67854f2036d
eight9080 <eight9080@gmail.com>
2018-03-28 02:18:06 +0300

    remove web.xml

---
+++ src/main/java/com/oreilly/security/initializer/AppWebAppInitializer.java
@@ -0,0 +1,25 @@
+package com.oreilly.security.initializer;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+public class AppWebAppInitializer implements WebApplicationInitializer{
+    @Override
+    public void onStartup(ServletContext servletContext) {
+        AnnotationConfigWebApplicationContext context
+                = new AnnotationConfigWebApplicationContext();
+
+        context.setConfigLocation("com.oreilly.security.config");
+
+        servletContext.addListener(new ContextLoaderListener(context));
+
+        final ServletRegistration.Dynamic dispatcher = servletContext
+                .addServlet("dispatcher", new DispatcherServlet(context));
+        dispatcher.addMapping("/");
+        dispatcher.setLoadOnStartup(1);
+    }
+}
