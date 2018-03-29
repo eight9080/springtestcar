6bcf844ee92b4ea413ac18ca6db9f082742932fc
eight9080 <eight9080@gmail.com>
2018-03-30 01:19:54 +0300

    form login

--- src/main/java/com/oreilly/security/config/SecurityConfig.java
+++ src/main/java/com/oreilly/security/config/SecurityConfig.java
@@ -35,18 +35,32 @@ public class SecurityConfig
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        //BASIC AUTH
 //        http.authorizeRequests()
 //                .anyRequest()
 //                .hasRole("USER")
 //                .and()
 //                .httpBasic();
+//        http.authorizeRequests()
+//                .antMatchers("/appointments/*").hasRole("USER")
+//                .antMatchers("/schedule/*").hasRole("FOO")
+////                .antMatchers("/*").hasRole("ANONYMOUS")
+//                    .and().httpBasic()
+//                .and()
+//             .authorizeRequests()
+//                .antMatchers("/*").hasRole("ANONYMOUS")
+//        ;
+
+        //FORM LOGIN
         http.authorizeRequests()
                 .antMatchers("/appointments/*").hasRole("USER")
                 .antMatchers("/schedule/*").hasRole("FOO")
-//                .antMatchers("/*").hasRole("ANONYMOUS")
-                    .and().httpBasic()
                 .and()
-             .authorizeRequests()
+                .formLogin().loginPage("/login").loginProcessingUrl("/login")
+                .usernameParameter("custom_username").passwordParameter("custom_password")
+
+                .and()
+                .authorizeRequests()
                 .antMatchers("/*").hasRole("ANONYMOUS")
         ;
     }
