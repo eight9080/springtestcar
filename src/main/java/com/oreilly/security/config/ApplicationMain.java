3ad2a6fdff6ad982d0d2da19cfda148c430357cf
eight9080 <eight9080@gmail.com>
2018-03-29 08:59:46 +0300

    spring boot

--- src/main/java/com/oreilly/security/config/ApplicationMain.java
+++ src/main/java/com/oreilly/security/config/ApplicationMain.java
@@ -1,11 +1,11 @@
-package com.oreilly.security;
+package com.oreilly.security.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.oreilly.security")
 public class ApplicationMain extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
